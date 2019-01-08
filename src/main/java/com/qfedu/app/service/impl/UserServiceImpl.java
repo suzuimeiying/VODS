30730198bd69df17a1a8eed0498b688be929ecce
zk <1690550060@qq.com>
2019-01-08 22:01:04 +0800

    解決beanBUG

--- src/main/java/com/qfedu/app/service/impl/UserServiceImpl.java
+++ src/main/java/com/qfedu/app/service/impl/UserServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
+@Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserLoginMapper userLoginDao;
