7af01f9f440be88525d6f0f0be508c1c06a39949
superman <zhych0828@126.com>
2019-01-11 11:50:45 +0800

    注册+商品+baby

--- src/main/java/com/qfedu/app/vo/JsonVo.java
+++ src/main/java/com/qfedu/app/vo/JsonVo.java
@@ -1,55 +1,42 @@
 package com.qfedu.app.vo;
 
-import java.util.HashMap;
-import java.util.Map;
 
-public class ResultVo {
-	private int code;
-	private String msg;
-	private Map<String, Object> data = new HashMap<>();
-
-	public int getCode() {
-		return code;
-	}
-
-	public void setCode(int code) {
-		this.code = code;
-	}
-
-	public String getMsg() {
-		return msg;
-	}
-
-	public void setMsg(String msg) {
-		this.msg = msg;
-	}
-
-	public Map<String, Object> getData() {
-		return data;
-	}
-
-	public void setData(Map<String, Object> data) {
-		this.data = data;
-	}
-
-	public ResultVo putDataValue(String key, Object value) {
-		data.put(key, value);
-		return this;
-	}
-
-	private ResultVo(int code, String message) {
-		this.code = code;
-		this.msg = message;
-	}
-
-	public static ResultVo setOK() {
-
-		return new ResultVo(1, "Ok");
-	}
-	public static ResultVo setERROR() {
-		return  new ResultVo(0, "ERROR");
-	}
+public class JsonVo {
+    private int code;
+    private String msg;
+    private Object data;
+    public int getCode() {
+        return code;
+    }
+    public void setCode(int code) {
+        this.code = code;
+    }
+    public String getMsg() {
+        return msg;
+    }
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+    public Object getData() {
+        return data;
+    }
+    public void setData(Object data) {
+        this.data = data;
+    }
 
 
-	
+    public static JsonVo setOK(Object data) {
+        JsonVo rvo=new JsonVo();
+        rvo.setCode(1);
+        rvo.setMsg("OK");
+        rvo.setData(data);
+        return rvo;
+    }
+    public static JsonVo setERROR() {
+        JsonVo rvo=new JsonVo();
+        rvo.setCode(0);
+        rvo.setMsg("ERROR");
+        return rvo;
+    }
+
 }
